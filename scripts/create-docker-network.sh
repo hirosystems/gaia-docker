ada5209ba3013c2d34aed21f98030f9d9e99c574
wileyj <wileyj@users.noreply.github.com>
2019-08-20 14:38:36 -0400

    updates

--- scripts/create-docker-network.sh
+++ scripts/create-docker-network.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -x
 BINARY="/usr/bin/docker"
 NETWORK="gaia"
-${BINARY} network inspect ${NETWORK} > /dev/null 2&>1
+${BINARY} network inspect ${NETWORK} > /dev/null 2>&1
 CHECK=$?
 if [[ $CHECK -ne 0 ]];then
   ${BINARY} network create -d bridge ${NETWORK}
