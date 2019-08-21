0bd3702c25551438eefa76cfa4fef882705ce21b
wileyj <wileyj@users.noreply.github.com>
2019-08-21 14:14:11 -0400

    cleanup

--- packer/build_images.sh
+++ packer/build_images.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
-if [ $1 ]; then
-  VERSION=$1
-else
-  VERSION=`/usr/local/bin/epoch`
-fi
+
 VERSION="2.5.3"
-# packer build --var-file=vars.json --var "user_data_file=gaia-ebs.ign" --var "version=0001.00.00" --var "ami_regions=us-west-2" gaia-ebs.json
-# packer build --var-file=vars.json --var "user_data_file=gaia-ephemeral.ign" --var "version=0001.00.00" --var "ami_regions=us-west-2" gaia-ephemeral.json
 
 echo "Setting Version to ${VERSION}"
 echo ""
@@ -15,6 +9,7 @@ echo "    user_data_file: gaia-ephemeral.ign"
 echo "    version: $VERSION"
 echo "    json: gaia-ephemeral.json"
 packer build --var-file=vars.json --var "user_data_file=gaia-ephemeral.ign" --var "version=$VERSION" gaia-ephemeral.json
+
 echo ""
 echo "Building gaia-hub-ebs"
 echo "    user_data_file: gaia-ebs.ign"
